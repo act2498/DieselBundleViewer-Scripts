1e1c23c42b30b27730c1a6972a3bd6b5443b5e00
Daniel R <luffydafloffi@gmail.com>
2020-09-21 16:54:54 +0300

    Added updated PackageOutputter script
    the file is used by the editor to generate its data

---
+++ PackageOutputter/main.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DieselBundleViewer;
+using DieselBundleViewer.Services;
+using DieselEngineFormats;
+using DieselEngineFormats.Bundle;
+using DieselEngineFormats.ScriptData;
+using System.Xml;
+
+public class main
+{
+    public void register()
+    {
+        Console.WriteLine("Register");
+        ScriptActions.RegisterScript(new PackageHashlistOutputter());
+    }
+}
+
+public class PackageHashlistOutputter
+{
+    public string key = "package_hashlist_outputter";
+    public string title = "Package Hashlist Outputter";
+
+    public void execute()
+    {
+        var browser = Utils.CurrentWindow;
+        using (StreamWriter file = new StreamWriter("packages.txt"))
+        {
+            foreach (KeyValuePair<Idstring, PackageHeader> pair in browser.PackageHeaders)
+            {
+                PackageHeader package = pair.Value;
+                if(package.Name.HasUnHashed)
+                {
+                    file.WriteLine("@"+package.Name);
+                    Console.WriteLine("Current pacakge " + package.Name.UnHashed);
+                }
+                else
+                {
+                    Console.WriteLine("Current pacakge " + package.Name.Hashed);
+                    Console.WriteLine("[Warning] no unhashed name, cannot add this pacakge to the list.");
+                }
+                foreach(PackageFileEntry entry in package.Entries)
+                {
+                    Idstring path = browser.DB.EntryFromID(entry.ID).Path;
+                    Idstring ext = browser.DB.EntryFromID(entry.ID).Extension;
+                    if(path.HasUnHashed)
+                        if(package.Name.HasUnHashed)
+                            file.WriteLine(path.UnHashed + "." + ext.UnHashed);
+                    else
+                        Console.WriteLine("File " + path.Hashed + " has no unhashed name, cannot add to the list.");
+                }
+            }
+        };
+    }
+}
